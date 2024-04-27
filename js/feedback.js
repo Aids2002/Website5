031a4a741829e45ebdf87e96af84244ef9d4c117
Aids2002 <146201117+Aids2002@users.noreply.github.com>
2024-04-27 16:44:21 +0800

    wew

---
+++ js/feedback.js
@@ -0,0 +1,46 @@
+const getLoginCont = document.querySelector('.loginContainer');
+const getRent = document.querySelector('#rent');
+const getLogo = document.querySelector('#logoContainer');
+const getFooter = document.querySelector('.footer');
+const getBottomContainer = document.querySelector('.bottomContainer')
+const getArrowContainer = document.querySelector('.arrowContainer');
+const getServices = document.querySelector('#services');
+
+const goMain = (variable) =>{
+    variable.addEventListener('click', () =>{
+        window.location.href = 'home.ejs'
+    })
+}
+goMain(getLogo);
+goMain(getRent);
+
+getLoginCont.addEventListener('click', () =>{
+    window.location.href = 'login.html'
+})
+
+let oldScrollStatus = 0;
+window.addEventListener('scroll', () =>{
+    const currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (currentScrollTop > oldScrollStatus){
+        getFooter.style.opacity = "0";
+        setTimeout(()=>{
+            getFooter.style.display ='none';
+        },300)
+    }
+    else{
+        setTimeout(()=>{
+            getFooter.style.opacity ='1';
+            getFooter.style.display ='flex';
+        },300)
+    }
+    oldScrollStatus = currentScrollTop;
+})
+
+
+
+getArrowContainer.addEventListener('click', () =>{
+    getBottomContainer.scrollIntoView ({behavior: 'smooth'});
+})
+getServices.addEventListener('click', () =>{
+    window.location.href = 'services.html'
+})
---
+++ js/servicess.js
@@ -0,0 +1,38 @@
+const getLoginCont = document.querySelector('.loginContainer');
+const getLogo = document.querySelector('#logoContainer');
+const getFooter = document.querySelector('.footer');
+const getRent = document.querySelector('#rent');
+const getFeedback = document.querySelector('#feedback');
+const goMain = (variable) =>{
+    variable.addEventListener('click', () =>{
+        window.location.href = 'home.ejs'
+    })
+}
+goMain(getLogo);
+goMain(getRent);
+
+
+let oldScrollStatus = 0;
+window.addEventListener('scroll', () =>{
+    const currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (currentScrollTop > oldScrollStatus){
+        getFooter.style.opacity = "0";
+        setTimeout(()=>{
+            getFooter.style.display ='none';
+        },300)
+    }
+    else{
+        setTimeout(()=>{
+            getFooter.style.opacity ='1';
+            getFooter.style.display ='flex';
+        },300)
+    }
+    oldScrollStatus = currentScrollTop;
+})
+
+getFeedback.addEventListener('click', () =>{
+    window.location.href = 'feedback.html'
+})
+getLoginCont.addEventListener('click', () =>{
+    window.location.href = 'login.html'
+})
